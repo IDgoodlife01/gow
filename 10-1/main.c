bd84f36fec522154bedbd8d111c912ee485bc9dd
hyeongjun <hyeong43210@naver.com>
2023-12-07 22:08:38 +0900

    커밋

--- 10-1/main.c
+++ 10-1/main.c
@@ -10,7 +10,7 @@ void main() {
 	/*
 	아래 의 PlaySound 메서드 는.Hay 음악 파일 을 재생 시켜 주는 함수 입니다.
 	* Controller.h 헤더 파일 에 include mmsystem.h 헤더 파일 을 통해.wav 음악 을 출력 시키는데,
-	*음악 을 출력 할 경우 해당 경로 를 Controller.h 에 BGM 의 경로 를 문자열 로 지정 하면 됩니다 .
+	*음악 을 출력 할 경우 해당 경로 를 Controller.h 에 BGM 의 경로 를 문자열 로 지정 하면 된다 .
 	* 그후 아래 PlaySound 주석 해제
 	* 음원 은 저자권 의 문제 로 수업 시간 에 따로 제공 되지 않습니다 .
 	*/
